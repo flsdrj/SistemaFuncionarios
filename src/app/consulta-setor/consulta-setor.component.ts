bdc413aca6eb4a47d93a0ad0916c4d5ad9122006
flsdrj <flsd_rj@hotmail.com>
2020-04-26 11:45:57 -0300

    implementado funcionalidade atualizar e excluir setores

--- src/app/consulta-setor/consulta-setor.component.ts
+++ src/app/consulta-setor/consulta-setor.component.ts
@@ -10,9 +10,19 @@ import { CookieService } from 'ngx-cookie-service';
 export class ConsultaSetorComponent implements OnInit {
 
   //atributos
+  setorEdicao = {
+    idSetor : 0,
+    nome : ''    
+  };
+  
   setores = []; //array vazio..
   endpointSetor = "http://localhost:53634/api/setor";
+  
   access_token = "";
+  headers:HttpHeaders;
+   
+  mensagem:string;
+  mensagemEdicao:string;
 
   //injeção de dependência
   constructor(private httpClient:HttpClient, 
@@ -23,14 +33,17 @@ export class ConsultaSetorComponent implements OnInit {
     
     //lendo o valor do token armazenado em cookie
     this.access_token = this.cookieService.get('access_token');
+
+    //montando o HEADER (cabeçalho) da requisição
+    this.headers = new HttpHeaders()
+      .set('Authorization', 'Bearer ' + this.access_token);
+
     this.consultarSetores();
   }
 
   consultarSetores(){
-    
-    //montando o HEADER (cabeçalho) da requisição
-    const headers = new HttpHeaders()
-      .set('Authorization', 'Bearer ' + this.access_token);
+        
+    const headers = this.headers;
 
     this.httpClient.get(this.endpointSetor, {headers})
       .subscribe(
@@ -38,6 +51,51 @@ export class ConsultaSetorComponent implements OnInit {
           this.setores = data;
         }
       );
+  } 
+
+  excluirSetor(id) {
+    if (window.confirm('Deseja realmente excluir o setor?')) {
+
+      const headers = this.headers;
+
+      this.httpClient.delete(this.endpointSetor + "/" + id, {headers})
+        .subscribe(
+          (data: any) => {
+            this.mensagem = data.mensagem;
+            this.consultarSetores();
+          }
+        );
+    }
+  }
+
+  exibirSetor(id){
+    
+    const headers = this.headers;
+
+    this.httpClient.get(this.endpointSetor + "/" + id, {headers} )
+      .subscribe(
+        (data: any) => {
+          this.setorEdicao = data;
+          this.mensagemEdicao = "";
+        }
+      );
+
+  }
+
+  atualizarSetor(formEdicao){
+
+    const headers = this.headers;
+    
+    this.mensagemEdicao = "Processando, por favor aguarde...";
+
+    this.httpClient.put(this.endpointSetor, formEdicao.value, {headers})
+    .subscribe(
+      (data: any) =>{
+        this.mensagemEdicao = data.mensagem;
+        this.consultarSetores();
+      }
+    );
+
   }
 
 }
